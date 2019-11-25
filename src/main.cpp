8d863a5c4b0ab3cf6d386ce2bddb32166f15bc27
yuyaushiro <yuya.suzuki0829@gmail.com>
2019-11-26 00:27:23 +0900

    Add rectangle drawing

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+
+int window_width = 640;
+int window_height = 480;
+
+
 int main(int argc, const char *argv[])
 {
   // GLFWの初期化
@@ -15,7 +20,7 @@ int main(int argc, const char *argv[])
   }
 
   // ウインドウの作成
-  GLFWwindow* window = glfwCreateWindow(640, 480, "Simple", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(window_width, window_height, "Simple", NULL, NULL);
   if (!window)
   {
     std::cerr << "Can't opwn window." << std::endl;
@@ -26,13 +31,28 @@ int main(int argc, const char *argv[])
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1);
 
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0.0f, window_width, 0.0f, window_height, -1.0f, 1.0f);
 
   // 描画のループ
   while (!glfwWindowShouldClose(window))
   {
     // バッファのクリア
-    glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+      // 四角形
+      static const GLfloat vtx4[] = {450.0f, 300.0f,
+                                     450.0f, 200.0f,
+                                     550.0f, 200.0f,
+                                     550.0f, 300.0f};
+      glVertexPointer(2, GL_FLOAT, 0, vtx4);
+      glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+
+      glEnableClientState(GL_VERTEX_ARRAY);
+      glDrawArrays(GL_LINE_LOOP, 0, 4);
+      glDisableClientState(GL_VERTEX_ARRAY);
 
     // ダブルバッファのスワップ
     glfwSwapBuffers(window);
